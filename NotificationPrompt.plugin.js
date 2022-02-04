9e7e9ca37b92aa366137bc5b76178751a231051d
onesketchyguy <lowebros9812@gmail.com>
2022-02-03 16:55:36 -0800

    Update NotificationPrompt.plugin.js

--- NotificationPrompt.plugin.js
+++ NotificationPrompt.plugin.js
@@ -1,53 +1,68 @@
 /**
  * @name NotificationPrompter
  * @description This is a plugin intended to prompt the user when joining a server with chat notifications default. 
- * @version 0.1.3
+ * @version 0.2.1
  * @author OneSketchyGuy
  * @authorLink https://github.com/onesketchyguy
  * @source https://github.com/onesketchyguy/NotificationPrompter/blob/main/NotificationPrompt.plugin.js
  * @updateUrl https://raw.githubusercontent.com/onesketchyguy/NotificationPrompter/main/NotificationPrompt.plugin.js
  * @website https://github.com/onesketchyguy/NotificationPrompter
  */
- 
+
+let MessageNotifications;
+let GuildStore;
+let SelectedGuildIDManager;
+
+const ARBITUARY_JOIN_TIME = 0.1;
+
  module.exports = class NotificationPrompter {
 	getName() { return "Notification prompter"; }
 	getAlertHead() { return "HEADS UP!"; }
-	getAlert() { return "This server has global notifications enabled."; }
-	//getUseAlert() { return this.useAlerts; }
+	getAlert(name) { return "This server has all message notifications enabled."; }
 	 
-	load()  { } // Optional function. Called when the plugin is loaded in to memory
-	start() { } // Required function. Called when the plugin is activated (including after reloads)
+	load()  { // Optional function. Called when the plugin is loaded in to memory
+
+		// Cache all the required modules
+		MessageNotifications = BdApi.findModuleByProps('resolvedMessageNotifications');
+		GuildStore = BdApi.findModuleByProps("getGuild"); 
+		SelectedGuildIDManager = BdApi.findModuleByProps("getLastSelectedGuildId");
+	}
+
+	start() { this.load() } // Required function. Called when the plugin is activated (including after reloads)
 	stop()  { } // Required function. Called when the plugin is deactivated
 
 	observer(changes) { } // Optional function. Observer for the `document`. Better documentation than I can provide is found here: <https://developer.mozilla.org/en-US/docs/Web/API/MutationObserver>
 
-	// getSettingsPanel() {
-		// this.useAlerts = !this.useAlerts;
-		
-		// if (this.useAlerts == true) BdApi.alert("Notificatins", "will appear like this.");
-		// else if (this.useAlerts == false) BdApi.showToast("Notificatins will appear like this.", {type:"info", icon: true});
-		// return null;
-	// }
-
 	onSwitch() {
 		// Get the ID from the current server/guild
-		var ID = BdApi.findModuleByProps("getLastSelectedGuildId").getLastSelectedGuildId();
-		
-		// Only show one notification per server/guild
-		if (ID == this.lastGuild) return;
-		this.lastGuild = ID;
-		
-		// Get the server/guild from the ID
-		var guild = BdApi.findModuleByProps("getGuild").getGuild(ID);
-		//console.log(guild);
+		var guildID = SelectedGuildIDManager.getLastSelectedGuildId();
+		console.log(guildID);
 
-		// FIXME: Add a check for if the user has overwritten the default notification settings
-		// FIXME: Add a check if this is the first time the user has joined this server
-		if (guild.defaultMessageNotifications == 0) { // On found server with Notify all messages flag
+		// Only show one notification per server/guild
+		if (guildID == this.lastGuildID) return;
+		this.lastGuildID = guildID;
+
+		// Get the server/guild from the ID
+		var guild = GuildStore.getGuild(guildID);
+
+		// Check for if the user has overwritten the default notification settings
+		var userSettings = MessageNotifications.getMessageNotifications(guildID);
+
+		 // On found server with Notify all messages flag
+		if (guild.defaultMessageNotifications == 0 && userSettings == 0) {
+			// Compare difference between the join date and current date
+			var joinDate = guild.joinedAt;
+			var diff = (Date.now()-joinDate)/86400000;
+			console.log(diff);
+
+			if (diff <= ARBITUARY_JOIN_TIME) {
+				BdApi.alert(this.getAlertHead(), this.getAlert());
+			}
+			else {
+				BdApi.showToast(this.getAlertHead() + " " + this.getAlert(), {type:"warning", icon: true});
+			}
+
 			// FIXME: Provide user with an option to disable the notification settings
-			//if (this.getUseAlert() == true) BdApi.alert(this.getAlertHead(), this.getAlert());
-			
-			BdApi.showToast(this.getAlertHead() + " " + this.getAlert(), {type:"warning", icon: true});
 		}
 	}
 }
